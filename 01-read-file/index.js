3ec8dea2199dcd2a25963d815ac5e3bc48174adf
Alla Borisova <alla.borisova27@gmail.com>
2022-05-21 12:07:21 +0500

    feat: add path to 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,6 @@
 
 const fs = require('fs');
-const readableStream = fs.createReadStream('01-read-file/text.txt', 'utf-8');
-readableStream.on('data', chunk => console.log(chunk))
+const path = require('path');
+const path_file = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(path_file, 'utf-8');
+readableStream.on('data', chunk => console.log(chunk));
